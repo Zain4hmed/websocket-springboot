822da83486fd6b980a01740772858b63d3915e94
zain ahmed <zainahmed01997@gmail.com>
2024-03-31 01:53:21 +0530

    might work this time

--- src/main/java/com/zainahme/chat/config/WebSocketConfig.java
+++ src/main/java/com/zainahme/chat/config/WebSocketConfig.java
@@ -1,12 +1,14 @@
-// WebSocketConfig.java
-
 package com.zainahme.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,5 +23,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        // Add Jackson message converter for JSON
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setPrettyPrint(true); // Enable pretty-printing of JSON
+        messageConverters.add(converter);
+        return true;
     }
 }
