d946b99cdb4bc81c82f19f7695dcfaefec825df9
zain ahmed <zainahmed01997@gmail.com>
2024-03-31 13:06:59 +0530

    lets see

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -24,7 +24,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('wss://websocket-springboot-production.up.railway.app/ws');
+        var socket = new SockJS('wss://websocket-springboot-production.up.railway.app/');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
