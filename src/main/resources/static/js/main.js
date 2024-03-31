04d195b61b7f9d68663950864d60228b52fbc22f
zain ahmed <zainahmed01997@gmail.com>
2024-03-31 13:17:25 +0530

    what works and what dose'nt

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -24,7 +24,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('ws://websocket-springboot-production.up.railway.app/');
+        var socket = new SockJS('https://websocket-springboot-production.up.railway.app/');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
