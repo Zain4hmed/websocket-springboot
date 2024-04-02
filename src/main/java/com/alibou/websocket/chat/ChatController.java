3b156fd7d951b9b626237a2db3977fcfb242d73c
zain ahmed <zainahmed01997@gmail.com>
2024-04-02 09:00:53 +0530

    added qr code and style changes

--- src/main/java/com/alibou/websocket/chat/ChatController.java
+++ src/main/java/com/alibou/websocket/chat/ChatController.java
@@ -1,13 +1,31 @@
 package com.alibou.websocket.chat;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Controller
 public class ChatController {
+
+    private static final String AppURL = "https://websocket-springboot-production.up.railway.app/";
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
@@ -26,5 +44,38 @@ public class ChatController {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
+    }
+
+    // mostly un-necessary but mostly usefull endpoints below.
+    @GetMapping(value = "/qrcode", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateQRCode() {
+        try {
+            // Generate QR code
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(AppURL, BarcodeFormat.QR_CODE, 250, 250);
+
+            // Convert QR code to byte array
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(bitMatrixToImage(bitMatrix), "png", byteArrayOutputStream);
+            byte[] imageData = byteArrayOutputStream.toByteArray();
+
+            // Return QR code image
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(imageData);
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private static BufferedImage bitMatrixToImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+            }
+        }
+        return image;
     }
 }
